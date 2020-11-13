92fa7bfed7ce5ef85fbfda73ccbb7f94a28e3689
Marko <29685366+MarkoTON@users.noreply.github.com>
2020-11-13 18:54:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  background: url('./assets/bg-image-cocktail.jpg');
+  background-position: center center;
+  background-size: cover;
+  height: 100vh;
+  overflow: auto;
+}
+
+#app::-webkit-scrollbar{
+  width:8px;
+}
+
+#app::-webkit-scrollbar-track {
+  background: #ddd;
+}
+
+#app::-webkit-scrollbar-thumb {
+  background: rgba(255, 255, 255,0.3);
+}
+
+#app::-webkit-scrollbar-track{
+  background: rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home container pt-3">
+    <div class="card mb-3" style="background:rgba(0,0,0,0.5)" v-for="(item,index) in info" :key="index">
+      <div class="row no-gutters">
+        <div class="col-md-4">
+          <img style="height:100%" :src="item.strDrinkThumb" class="card-img" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body text-white text-shadow">
+            <h5 class="card-title ">{{item.strDrink}}</h5>
+            <p class="card-text ">{{item.strInstructions}}</p>
+            <p class="">Ingredients:</p>
+            <ul class="">
+              <li class="" v-if="item.strIngredient1">{{item.strIngredient1}} : {{ item.strMeasure1 }}</li>
+              <li class="" v-if="item.strIngredient2">{{item.strIngredient2}} : {{ item.strMeasure2 }}</li>
+              <li class="" v-if="item.strIngredient3">{{item.strIngredient3}} : {{ item.strMeasure3 }}</li>
+              <li class="" v-if="item.strIngredient4">{{item.strIngredient4}} : {{ item.strMeasure4 }}</li>
+              <li class="" v-if="item.strIngredient5">{{item.strIngredient5}} : {{ item.strMeasure5 }}</li>
+              <li class="" v-if="item.strIngredient6">{{item.strIngredient6}} : {{ item.strMeasure6 }}</li>
+              <li class="" v-if="item.strIngredient7">{{item.strIngredient7}} : {{ item.strMeasure7 }}</li>
+              <li class="" v-if="item.strIngredient8">{{item.strIngredient8}} : {{ item.strMeasure8 }}</li>
+              <li class="" v-if="item.strIngredient9">{{item.strIngredient9}} : {{ item.strMeasure9 }}</li>
+              <li class="" v-if="item.strIngredient10">{{item.strIngredient10}} : {{ item.strMeasure10 }}</li>
+              <li class="" v-if="item.strIngredient11">{{item.strIngredient11}} : {{ item.strMeasure11 }}</li>
+              <li class="" v-if="item.strIngredient12">{{item.strIngredient12}} : {{ item.strMeasure12 }}</li>
+              <li class="" v-if="item.strIngredient13">{{item.strIngredient13}} : {{ item.strMeasure13 }}</li>
+              <li class="" v-if="item.strIngredient14">{{item.strIngredient14}} : {{ item.strMeasure14 }}</li>
+              <li class="" v-if="item.strIngredient15">{{item.strIngredient15}} : {{ item.strMeasure15 }}</li>
+            </ul>
+            <p class="">Glass: {{ item.strGlass }}</p>
+            <p class="">Category: {{ item.strCategory }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      info:null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a')
+      .then(response => (this.info = response.data.drinks))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.text-shadow {
+  text-shadow: 1px 1px 1px rgba(0,0,0,0.8);
+}
+</style>
